6302ba8d272ed50d404ca29ba4b3e63dc571a42c
Toth Alexandra <atoth1433@gmail.com>
2026-01-17 23:01:15 +0200

    Added mapping for register to generation and fast api file
    Introduces a FastAPI-based API for honey encryption with endpoints for registration, login, user registry, and health checks. Adds core logic for honeywords and fake data generation in he.py, a requirements.txt for dependencies, a sample honey_users.json, and truncates honey_data.json for brevity.

--- backend/honey_generator.py
+++ backend/honey_generator.py
@@ -1,6 +1,6 @@
 import json
 import random
-
+from pydantic_extra_types.payment import PaymentCardNumber, PaymentCardBrand
 try:
     from faker import Faker
 except ImportError:
@@ -8,18 +8,59 @@ except ImportError:
     exit()
 
 fake = Faker()
-
-def generate_honey_data(number_of_entries=1000):
+import datetime
+def generate_honey_data(number_of_entries=1000,real_data=None):
     honey_database = []
+    ALLOWED_FAKER_BRANDS = ['visa', 'mastercard', 'amex', 'discover']
     print(f"Generating {number_of_entries} fake bank data...")
     for _ in range(number_of_entries):
         balance_int = random.randint(50, 15000)
         balance_cents = random.randint(10, 99)
-
+        brand='mastercard'
+        if real_data and "card_number" in real_data:
+            detected_brand = PaymentCardNumber(real_data["card_number"]).brand.value
+            if detected_brand in ALLOWED_FAKER_BRANDS:
+                brand = detected_brand
+            else:
+                brand = random.choice(ALLOWED_FAKER_BRANDS)
+   
+        if real_data:
+            real_data_exp=real_data.get("expiration_date")
+            if real_data_exp:
+                real_year = int(real_data_exp.split('/')[1])
+                # Add random variance of -3 to +3 years
+                variance = random.randint(-2, 2)
+                end_year = real_year + variance
+            else:
+                end_year = datetime.datetime.now().year + 10
+            if len(end_year.__str__())==2:
+                end_year = 2000 + end_year
+            
+            # Slightly modify real data to create plausible honey entries
+            entry = {
+                "account_ID": fake.uuid4(),
+                "full_name": real_data.get("full_name",'').split()[0] + " " + fake.last_name(),
+                "email": fake.email(),
+                "card_number": fake.credit_card_number(card_type=brand),
+                "balance": f"{balance_int}.{balance_cents} RON",
+                "ccv": fake.credit_card_security_code(card_type=brand),
+                "expiration_date": fake.credit_card_expire(end=datetime.date(end_year, 12, 31)),
+                "brand": brand
+                
+            }
+            # de vazut daca are acelasi numar ca si cel real (daca da, il sarim sau il lasam ca e mai confuzant?)
+            honey_database.append(entry)
+            continue
         # Generating plausible card number data using faker
         entry = {
-            "card_number": fake.credit_card_number(card_type='mastercard'),
-            "balance": f"{balance_int}.{balance_cents} RON"
+            "account_ID": fake.uuid4(),
+            "full_name": fake.name(),
+            "email": fake.email(),
+            "card_number": fake.credit_card_number(card_type=brand),
+            "balance": f"{balance_int}.{balance_cents} RON",
+            "ccv": fake.credit_card_security_code(card_type=brand),
+            "expiration_date": fake.credit_card_expire(),
+            "brand": brand
         }
         honey_database.append(entry)
     return honey_database
